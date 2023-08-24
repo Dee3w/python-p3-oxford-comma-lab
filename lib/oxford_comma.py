385c63e90243a1283b4373cf2a78dacc8fb68d98
Dee3w <joydanah17@gmail.com>
2023-08-24 14:22:06 +0300

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+     if len(items) == 1:
+        return items[0]
+     elif len(items) == 2:
+        return " and ".join(items)
+     elif len(items) > 2:
+        new_list = items
+        a = 'and ' + str(new_list[-1])
+        new_list[-1] = a
+        return ", ".join(new_list)
+     else:
+        return "List provided is empty."
